64e2093f0f6741fc6c929be55b4532accbd5681b
dlam15 <31551025+dlam15@users.noreply.github.com>
2021-02-06 17:08:15 -0500

    redid home screen

--- controller.py
+++ controller.py
@@ -1,11 +1,11 @@
-from startScreen import startScreenBegin
+import StartScreen
 
-#Call controller to start game
 
-#first we open start screen
-if startScreenBegin() == True:
-    print("starting game")
-
+class Controller:
+    #first we open start screen
+    start = StartScreen.StartScreen()
+    if start.startScreenBegin() == True:
+        print("starting game")
 
 #next we'll open character select
 #we'll return vals to pass to game (which characters)
@@ -13,3 +13,7 @@ if startScreenBegin() == True:
 #then we'll play intro sequence
 
 #then start game
+
+
+#Call controller to start game
+Controller()
